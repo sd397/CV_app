fa58fbe9037f81b0eb24158e4d1e254432e327f4
Szymon Dawidow <>
2020-06-17 18:56:56 +0200

    Git init

---
+++ CVapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CVapp
+//
+//  Created by Szymon Dawidow on 17/06/2020.
+//  Copyright © 2020 Szymon Dawidow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
