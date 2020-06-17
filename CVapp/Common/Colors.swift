b03e05911fda593d6c3942ee782324eec6f35e8e
Szymon Dawidow <>
2020-06-17 21:42:03 +0200

    Add colors

---
+++ CVapp/Common/Colors.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+public struct Colors {
+	public static let background: UIColor = color(named: "background")
+	public static let lightGray: UIColor = color(named: "lightGray")
+	public static let mainText: UIColor = color(named: "mainText")
+	public static let subeheader: UIColor = color(named: "subeheader")
+	
+	static func color(named: String) -> UIColor {
+		if let color = UIColor(named: named) {
+			return color
+		} else {
+			print(" --- Cannot find color named \(named)")
+			return .cyan
+		}
+	}
+}
