b2d51b8734046153b633cbf3678207375837bb4e
Szymon Dawidow <>
2020-06-18 08:40:31 +0200

    Add constants

---
+++ CVapp/Common/Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public struct Constants {
+	
+	public struct Margin {
+		public static let tiny: CGFloat = 8
+		public static let small: CGFloat = 12
+		public static let regular: CGFloat = 24
+		public static let big: CGFloat = 40
+	}
+}
